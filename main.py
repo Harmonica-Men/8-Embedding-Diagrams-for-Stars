438c7c987816580c4773e5d0040c274bb2f49cdf
filip.vanelslande@gmail.com <filip.vanelslande@gmail.com>
2023-12-24 16:50:45 +0100

    first code template

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
+import matplotlib.pyplot as plt
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+M = 20 # Mass of the Star #
+R1 = 2.0 # Radius of Star
+r = 1 # Radial vector
+z = 0 # Amplitude of plot
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+data = []
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/ d
+for M in range(2, 11, 1)
+for M in [0.2 + i * 0.1 for i in range(9)]:
+    for R2 in [0.1 + i * 0.2 for i in range(46)]:
+        # inside star #
+        if abs(R2) <= R1:
+            z = math.sqrt((R1 * R1 * R1) / (2.0 * M)) * (1 - math.sqrt(1 - 2.0 * M * r * r) / (R * R * R))
+        # outside star #
+        if abs(R2) >= R1:
+            z = math.sqrt((R * R * R) / (2.0 * M)) * (1 - math.sqrt(1 - 2.0 * M / R)) + math.sqrt(
+                8.0 * M * (abs(r) - 2.0 * M)) - math.sqrt(8 * M * (R - 2 * M))
+
+        data.append((r, z))
+
+r_vals, z_vals = zip(*data)
+plt.plot(r_vals, z_vals)
+plt.xlabel('Radial Vector (r)')
+plt.ylabel('Amplitude (z)')
+plt.title('Cross Section of Embedding Diagrams for Stars of Different Masses')
+plt.grid(True)
+plt.show()
+
