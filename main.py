c794b87b476b2c99107189113cabed402131026d
filip.vanelslande@gmail.com <filip.vanelslande@gmail.com>
2023-12-24 17:15:06 +0100

    test code

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
 import math
-import matplotlib.pyplot as plt
 
+R1 = 2
 
+for M in range(2, 11, 1):
+    M /= 10.0  # Convert to floating-point value
 
-M = 20 # Mass of the Star #
-R1 = 2.0 # Radius of Star
-r = 1 # Radial vector
-z = 0 # Amplitude of plot
-
-data = []
-
-for M in range(2, 11, 1)
-for M in [0.2 + i * 0.1 for i in range(9)]:
-    for R2 in [0.1 + i * 0.2 for i in range(46)]:
-        # inside star #
+    for R2 in [i * 0.2 - 10 for i in range(101)]:  # Equivalent to STEP 0.2 from -10 to 10
+        # inside of the star #
         if abs(R2) <= R1:
-            z = math.sqrt((R1 * R1 * R1) / (2.0 * M)) * (1 - math.sqrt(1 - 2.0 * M * r * r) / (R * R * R))
-        # outside star #
-        if abs(R2) >= R1:
-            z = math.sqrt((R * R * R) / (2.0 * M)) * (1 - math.sqrt(1 - 2.0 * M / R)) + math.sqrt(
-                8.0 * M * (abs(r) - 2.0 * M)) - math.sqrt(8 * M * (R - 2 * M))
+            Z = math.sqrt((R1 * R1 * R1) / (2 * M)) * (1 - math.sqrt(1 - 2.0 * M * R2 * R2 / (R1 * R1 * R1)))
+        else:
+            Z = math.sqrt((R1 * R1 * R1) / (2 * M)) * (1 - math.sqrt(1 - 2 * M / R1)) + math.sqrt(8 * M * (abs(R2) - 2.0 * M)) - math.sqrt(8 * M * (R1 - 2.0 * M))
 
-        data.append((r, z))
-
-r_vals, z_vals = zip(*data)
-plt.plot(r_vals, z_vals)
-plt.xlabel('Radial Vector (r)')
-plt.ylabel('Amplitude (z)')
-plt.title('Cross Section of Embedding Diagrams for Stars of Different Masses')
-plt.grid(True)
-plt.show()
-
+        print(R2, Z)
